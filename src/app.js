7c0392ce8ee619d0e1c8051bde6a74c31e1fdc00
Yeseniya07 <108342677+Yeseniya07@users.noreply.github.com>
2022-08-05 01:13:52 +0300

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
+function formatDay(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(responce) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(responce.data.main.temp);
@@ -13,6 +29,9 @@ function displayTemperature(responce) {
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(responce.data.wind.speed);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDay(responce.data.dt * 1000);
 }
 let apiKey = "2acfad573be44e3b6850ce49fb547535";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
