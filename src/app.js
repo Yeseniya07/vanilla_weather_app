cfde6a345abc3bd98bbe8d6156881f2961ed303e
Yeseniya07 <108342677+Yeseniya07@users.noreply.github.com>
2022-08-05 13:55:56 +0300

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -40,8 +40,20 @@ function displayTemperature(responce) {
   );
   iconElement.setAttribute("alt", responce.data.weather[0].description);
 }
-let apiKey = "2acfad573be44e3b6850ce49fb547535";
-let city = "Zhytomyr";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("New York");
